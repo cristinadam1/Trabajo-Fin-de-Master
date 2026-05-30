cce9472799f3264263d267a8c3e1293cde88cdd3
cristinadam1 <cristinadam@correo.ugr.es>
2026-05-30 15:18:44 +0200

    fix: extra=ignore añadido

--- AgenteVerificador/app/config.py
+++ AgenteVerificador/app/config.py
@@ -2,13 +2,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
     ENV: str = "development"
     PROJECT_NAME: str = "Agente Verificador de Seguridad"
     DATABASE_URL: str
     LLM_API_KEY: str
     LLM_MODEL_NAME: str = "llama-guard3:1b"
+    ALLOWED_TOOLS: str = "read_current_time,get_weather_info,view_documentation"
 
 
 settings = Settings()
